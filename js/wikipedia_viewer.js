d8ad17f06e710a6d219f2e919ed739d388d32f98
Stepan Yefimov <stepan.yefimov@gmail.com>
2017-01-17 15:02:59 +0200

    Functionally everything works. Styling remains to be done

--- js/wikipedia_viewer.js
+++ js/wikipedia_viewer.js
@@ -1,6 +1,3 @@
-
-
-
 // All comments are for not experienced peoples like me now
 
 //Make a variable cors_api_url which now contains url 'https://cors-anywhere.herokuapp.com/'
@@ -26,31 +23,49 @@
     x.send();
   }
   //Declare function getQuote
-  function getWiki() {
+  function getWiki(event) {
   		// Declare variable with our url with quote
-		var wikiUrl = "https://en.wikipedia.org/w/api.php?action=query&titles=obama&prop=revisions&rvprop=contentt&format=json";
+		var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + event.target.value;
       // Call our first function doCORSRequest and give it as agrument an object with method and url
       // function printResult parses respond and print a result in to html 
       doCORSRequest({
         method: 'GET',
         url: wikiUrl,
-      }, function printResult(result) {
-      		//var $json = JSON.parse(result); 
-       		$(".json").html(JSON.stringify(result));
-      });
+      }, printResult);
 
+  }
+	function printResult(result) {
+      		var $json = JSON.parse(result); 
+		for(var i = 0, n = $json[1].length; i < n; i++){
+			var $newLink = $('<a href="'+$json[3][i]+'" target="_blank"><h3>'+$json[1][i]+'</h3></a>');
+			var $newText = $('<p>' + $json[2][i] +'</p>');
+			var $newDiv = $('<div class="cell"></div>');
+			$newDiv.appendTo(".result");
+			$newLink.appendTo($newDiv);
+			$newText.insertAfter($newLink);		
+		}  
   }
   
 
 
 $( document ).ready(function(){
-	// Unbinding a particular click handler, using a reference to the function
-var foo = function(event) {
- 
-	console.log(event);
-	console.log(event.target.value);
-}
- 
-$( "p" ).on( "change", foo );
+	
+	$( "p" ).on( "change", getWiki );
+	$("#text-field").autocomplete({
+	    source: function(request, response) {
+	        $.ajax({
+	            url: "http://en.wikipedia.org/w/api.php",
+	            dataType: "jsonp",
+	            data: {
+	                'action': "opensearch",
+	                'format': "json",
+	                'search': request.term
+	            },
+	            success: function(data) {
+	                response(data[1]);
+	            }
+	        });
+	    }
+	});
 
 });
