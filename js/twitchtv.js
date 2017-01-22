c8b6c699395c1aef642bedca8114e4d0c37cc791
Stepan Yefimov <stepan.yefimov@gmail.com>
2017-01-22 22:38:31 +0200

    Created one variable for each for loop with the let keyword and used trick from the YouDon'tKnow JS book. Also used jquery selector :eq(index). Маde requests and succesfully write the result to the page.

--- js/twitchtv.js
+++ js/twitchtv.js
@@ -1,15 +1,43 @@
 // The variable which contains streams list
-var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin",
+var streams1 = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin",
 	"comster404"];
+var streams = ["ESL_SC2", "freecodecamp"];
+var workUrl = 'https://wind-bow.gomix.me/twitch-api';
 $( document ).ready(function(){
-/*	for(var i = 0, n = streams.length; i < n; i++) {
-		$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + streams[i] + '?callback=?', function(data) {
-	  		$(".result").append('<div class="row"><div class="col-xs-2"><img class="logo" src="' + data.logo
-		+ '"></div><div class="col-xs-3 display_name">' + data.display_name
-		+ '</div><div class="col-xs-7"></div></div>');
-	  	});	
-	}
-	*/
+	for(var i = 0, n = streams.length; i < n; i++) {
+		let rowNum = i;
+		$.getJSON(workUrl + '/channels/' + streams[i] + '?callback=?', function(data) {
+			if(data.logo){
+				$(".result").append('<div class="row"><div class="col-xs-2"><img class="logo" src="' + data.logo + '"></div>');
+			}
+			else {
+				$(".result").append('<div class="row"><div class="col-xs-2"></div>');	
+			};
+			if (data.display_name) {
+				$(".row:last").append('<div class="col-xs-2">' + data.display_name + '</div>');
+			}
+			else{
+				$(".row:last").append('<div class="col-xs-2">' + streams[i] + '</div>');
+			}
+			if (data.error) {
+	// appending the status
+				$(".row:last").append('<div class="col-xs-5">' + data.status + ' ' + data.message + '</div>');
+				$(".row:last").append('<div class="col-xs-3">' + data.error + '</div>');
+			}
+			else {
+				$(".row:last").append('<div class="col-xs-5">' + data.status + '</div>');
+				console.log(rowNum);
+				$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + streams[i] + '?callback=?', function(data) {
+					if (data.stream !== null) {	 				
+	  					$(".row").eq(rowNum).append('<div class="col-xs-3">Online</div>');
+					}
+					else {
+						$(".row").eq(rowNum).append('<div class="col-xs-3">Offline</div>');
+					};	
+				});
+			};	
+		});	
+
 	
 /*	for(var i = 0, n = streams.length; i < n; i++) {
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + streams[i] + '?callback=?', function(data) {
@@ -18,16 +46,17 @@ $( document ).ready(function(){
 	}*/
 	
 
-		$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/ESL_SC2?callback=?', function(data) {
+/*		$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/ESL_SC2?callback=?', function(data) {
 	  console.log(data);
 	});
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?', function(data) {
 	  console.log(data);
 	});
-			$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/brunofin?callback=?', function(data) {
+			$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/brunofin?callback=?', function(data) {
 	  console.log(data);
-	});
+	}); 
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/comster404?callback=?', function(data) {
 	  console.log(data);
-	});
+	});*/
+	}
 });
