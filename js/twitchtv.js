59bf3bf450b62bf15bea78101e741ac0e856c49a
Stepan Yefimov <stepan.yefimov@gmail.com>
2017-01-23 21:09:34 +0200

    Fixed the bug so at the moment script displays really correct information.

--- js/twitchtv.js
+++ js/twitchtv.js
@@ -1,45 +1,57 @@
 // The variable which contains streams list
-var streams1 = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin",
+var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin",
 	"comster404"];
-var streams = ["ESL_SC2", "freecodecamp"];
+var streams1 = ["ESL_SC2", "freecodecamp"];
 var workUrl = 'https://wind-bow.gomix.me/twitch-api';
 $( document ).ready(function(){
 	for(var i = 0, n = streams.length; i < n; i++) {
 		let rowNum = i;
-		$.getJSON(workUrl + '/channels/' + streams[i] + '?callback=?', function(data) {
+		$(".result").append('<div class="row"></div>');
+		$.getJSON(workUrl + '/channels/' + streams[rowNum] + '?callback=?', function(data) {
 			if(data.logo){
-				$(".result").append('<div class="row"><div class="col-xs-2"><img class="logo" src="' + data.logo + '"></div>');
+				$(".row").eq(rowNum).append('<div class="col-xs-2"><img class="logo" src="' + data.logo + '"></div>');
 			}
 			else {
-				$(".result").append('<div class="row"><div class="col-xs-2"></div>');	
+				$(".row").eq(rowNum).append('<div class="col-xs-2"><img class="logo" src="http://res.cloudinary.com/dtnyso8nn/image/upload/v1485194092/tvitchtv/blanck_logo1.png"></div>');	
 			};
 			if (data.display_name) {
-				$(".row:last").append('<div class="col-xs-2">' + data.display_name + '</div>');
+				$(".row").eq(rowNum).append('<div class="col-xs-2">' + data.display_name + '</div>');
 			}
 			else{
-				$(".row:last").append('<div class="col-xs-2">' + streams[i] + '</div>');
+				$(".row").eq(rowNum).append('<div class="col-xs-2">' + streams[rowNum] + '</div>');
 			}
 			if (data.error) {
 	// appending the status
-				$(".row:last").append('<div class="col-xs-5">' + data.status + ' ' + data.message + '</div>');
-				$(".row:last").append('<div class="col-xs-3">' + data.error + '</div>');
+				$(".row").eq(rowNum).append('<div class="col-xs-5">' + data.status + '. ' + data.message + '</div>');
+				$(".row").eq(rowNum).append('<div class="col-xs-3">' + data.error + '</div>');
 			}
 			else {
-				$(".row:last").append('<div class="col-xs-5">' + data.status + '</div>');
-				console.log(rowNum);
-				$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + streams[i] + '?callback=?', function(data) {
-					if (data.stream !== null) {	 				
-	  					$(".row").eq(rowNum).append('<div class="col-xs-3">Online</div>');
-					}
-					else {
-						$(".row").eq(rowNum).append('<div class="col-xs-3">Offline</div>');
-					};	
-				});
+				if(jQuery.isEmptyObject(data.status)){
+					$(".row").eq(rowNum).append('<div class="col-xs-5"></div>');
+					$.getJSON(workUrl + '/streams/' + streams[rowNum] + '?callback=?', function(data) {
+						if (jQuery.isEmptyObject(data.stream)) {	 				
+	  						$(".row").eq(rowNum).append('<div class="col-xs-3">Offline</div>');
+						}
+						else {
+							$(".row").eq(rowNum).append('<div class="col-xs-3">Online</div>');
+						};
+					});	
+				}
+				else {
+					$(".row").eq(rowNum).append('<div class="col-xs-5">' + data.status + '</div>');
+					$.getJSON(workUrl + '/streams/' + streams[rowNum] + '?callback=?', function(data) {
+						if (jQuery.isEmptyObject(data.stream)) {	 				
+	  						$(".row").eq(rowNum).append('<div class="col-xs-3">Offline</div>');
+						}
+						else {
+							$(".row").eq(rowNum).append('<div class="col-xs-3">Online</div>');
+						};
+					});	
+				};
 			};	
-		});	
-
-	
-/*	for(var i = 0, n = streams.length; i < n; i++) {
+		});
+	}
+	/*	for(var i = 0, n = streams.length; i < n; i++) {
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + streams[i] + '?callback=?', function(data) {
 	  		response[i] = data;
 		});		
@@ -58,5 +70,4 @@ $( document ).ready(function(){
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/comster404?callback=?', function(data) {
 	  console.log(data);
 	});*/
-	}
 });
